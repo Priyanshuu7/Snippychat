aa9883ce800f4b9de63ab017676d0b9e2898c453
Priyanshuu7 <rajakpriyanshu581@gmail.com>
2025-06-20 14:32:26 +0530

    redis url config

--- server/src/config/redis.config.ts
+++ server/src/config/redis.config.ts
@@ -1,9 +1,5 @@
 import { Redis } from "ioredis";
 
-const redis = new Redis({
-  host: "localhost",
-  port: 6379,
-});
-
+const redis = new Redis(process.env.REDIS_URL as string);
 export default redis;
  
